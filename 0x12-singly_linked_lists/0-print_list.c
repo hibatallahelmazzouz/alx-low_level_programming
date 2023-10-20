ba9f42658c1a0f87d530d3a12c7764ba329b9db6
hibatallahelmazzouz <elmazzouzhiba@gmail.com>
2023-10-20 14:14:59 -0700

    created

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,37 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-
 /**
-* print_list - check the code for Holberton School students.
-* @h: name of the list
-* Return: the number of nodes.
+ * print_list - print elements in a linked list
+ *
+ * @h: pointer to head first node
+ *
+ * Return: Number of nodes
 */
+
 size_t print_list(const list_t *h)
 {
-   int count = 0;
+	int node_count = 1;
 
+	/*return 0 as number of nodes*/
+	if (h == NULL)
+		return (0);
 
-   while (h)
-   {
-       if (h->str == NULL)
-       {
-           printf("[0] (nil)\n");
-       }
-       else
-       {
-       printf("[%d] %s\n", h->len, h->str);
-       }
-       count++;
-       h = h->next;
-   }
-   return (count);
+	while (h->next != NULL)
+	{
+		/*if str is NULL return len as 0 and str as (nil)*/
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		/*go to the next node*/
+		h = h->next;
+		/*count number of nodes*/
+		node_count += 1;
+	}
+
+	/*print for the last node*/
+	printf("[%d] %s\n", h->len, h->str);
+
+	return (node_count);
 }
-
